e80ca78248b1a1d59375887af86b1c46b9e9b4f0
AxelNero <evendanker.memes@gmail.com>
2021-04-03 21:01:49 +0200

    Create Weapon.cs

---
+++ G01_Perseus/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G01_Perseus
+{
+    public class Weapon : Equipment
+    {
+        protected float rateOfFire;
+        protected float timeSinceLastFire;
+
+        public Weapon(int iD) : base(iD)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (timeSinceLastFire <= rateOfFire)
+            {
+                timeSinceLastFire += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        public virtual void Fire(Vector2 center, Vector2 target, float rotation)
+        {
+
+        }
+    }
+}
