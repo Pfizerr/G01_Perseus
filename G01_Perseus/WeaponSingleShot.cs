f8595e7e3671819c7ee7d0e5c09667b92b019e9f
AxelNero <evendanker.memes@gmail.com>
2021-04-13 18:03:07 +0200

    Revert "Weapon System"
    This reverts commit d92d07cf2186644d91c0d0540c3d8973a01a76c7.

--- G01_Perseus/WeaponSingleShot.cs
+++ G01_Perseus/WeaponSingleShot.cs
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 ﻿using System;
-=======
-﻿using Microsoft.Xna.Framework;
-using System;
->>>>>>> parent of be71ea6 (Revert "Merge branch 'Weapon1' into main")
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,30 +6,7 @@ using System.Threading.Tasks;
 
 namespace G01_Perseus
 {
-<<<<<<< HEAD
     class WeaponSingleShot
     {
-=======
-    public class WeaponSingleShot : Weapon
-    {
-        public WeaponSingleShot(int iD, float powerLevel) : base(iD, powerLevel)
-        {
-            name = "Singleshot";
-            rateOfFire = 1f;
-            baseDamagePerShot = 5;
-            damagePerShot = baseDamagePerShot * powerLevel;
-        }
-
-        public override void Fire(Vector2 center, Vector2 target, float rotation)
-        {
-            if (timeSinceLastFire >= rateOfFire)
-            {
-
-                EntityManager.AddBullet(new Bullet(center, target, 7f, Color.Red, new Point(20, 20), 10, false));
-                
-                timeSinceLastFire = 0;
-            }
-        }
->>>>>>> parent of be71ea6 (Revert "Merge branch 'Weapon1' into main")
     }
 }
--- G01_Perseus/WeaponTripleShot.cs
+++ G01_Perseus/WeaponTripleShot.cs
@@ -3,44 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 
 namespace G01_Perseus
 {
     class WeaponTripleShot
     {
-=======
-using Microsoft.Xna.Framework;
-
-namespace G01_Perseus
-{
-    public class WeaponTripleShot : Weapon
-    {
-        private Vector2 secondBulletTarget, thirdBulletTarget;
-        public WeaponTripleShot(int iD, float powerLevel) : base(iD, powerLevel)
-        {
-            name = "Tripleshot";
-            rateOfFire = 1.5f;
-            baseDamagePerShot = 2;
-            damagePerShot = baseDamagePerShot * powerLevel;
-        }
-
-        public override void Fire(Vector2 center, Vector2 target, float rotation)
-        {
-            if (timeSinceLastFire >= rateOfFire)
-            {
-                secondBulletTarget.X = (float)Math.Cos(rotation - (float)Math.PI * 1.1f) * 100 + center.X;
-                secondBulletTarget.Y = (float)Math.Sin(rotation - (float)Math.PI * 1.1f) * 100 + center.Y;
-                thirdBulletTarget.X = (float)Math.Cos(rotation + (float)Math.PI * 1.1f) * 100 + center.X;
-                thirdBulletTarget.Y = (float)Math.Sin(rotation + (float)Math.PI * 1.1f) * 100 + center.Y;
-
-                EntityManager.AddBullet(new Bullet(center, target, 7f, Color.Red, new Point(20, 20), 10, false));
-                EntityManager.AddBullet(new Bullet(center, secondBulletTarget, 7f, Color.Red, new Point(20, 20), 10, false));
-                EntityManager.AddBullet(new Bullet(center, thirdBulletTarget, 7f, Color.Red, new Point(20, 20), 10, false));
-                timeSinceLastFire = 0;
-            }
-        }
-
->>>>>>> parent of be71ea6 (Revert "Merge branch 'Weapon1' into main")
     }
 }
