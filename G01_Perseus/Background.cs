0d84dff47bfa2d1edc5bc203fea2c303ba37bb31
Joakim Ekbladh <joakim.ekbladh@gmail.com>
2021-06-01 14:05:37 +0200

    parallax

--- G01_Perseus/Background.cs
+++ G01_Perseus/Background.cs
@@ -11,32 +11,23 @@ namespace G01_Perseus
 {
     class Background
     {
-        private List<Vector2> front, middle, back;
-        private int frontSpace, middleSpace, backSpace;
-        private float frontSpeed, middleSpeed, backSpeed;
         private Texture2D[] tex;
-        private GameWindow window;      
 
-        public Background()
+        public Background(int tileWidth, int tileHeight)
         {
             this.tex = new Texture2D[3];
-            tex[0] = AssetManager.TextureAsset("space_background");
-            tex[1] = AssetManager.TextureAsset("space_background");
-            tex[2] = AssetManager.TextureAsset("space_background");
-            tex[0] = Util.CreateSpaceTexture(0, tex[1].Width, tex[1].Height, 1000, 1, 10);
+            tex[0] = Util.CreateSpaceTexture(0, tileWidth, tileHeight, 100, 10,15);
+            tex[1] = Util.CreateSpaceTexture(0, tileWidth, tileHeight, 1000, 1, 5);
+            tex[2] = Util.CreateSpaceTexture(1, tileWidth, tileHeight, 100, 5, 7);
 
-            front = new List<Vector2>();
-            frontSpace = tex[0].Width / 5;
-            frontSpeed = 0.75f;
-
-            back = new List<Vector2>();
-            back.Add(new Vector2(0));
-            backSpeed = 0.25f;
         }
 
-        public void Draw(SpriteBatch sb, Rectangle bounds)
-        {           
-            sb.Draw(tex[0], bounds, Color.White);                  
+        public void Draw(SpriteBatch sb, Rectangle bounds, Vector2 cameraPosition)
+        {
+            sb.Draw(tex[0], new Vector2(bounds.X, bounds.Y) - cameraPosition * 0.0001f, Color.White);
+            sb.Draw(tex[1], new Vector2(bounds.X, bounds.Y) - cameraPosition * 0.05f, Color.White);
+            sb.Draw(tex[2], new Vector2(bounds.X, bounds.Y) - cameraPosition * 0.1f, Color.White);
+
         }
 
     }
